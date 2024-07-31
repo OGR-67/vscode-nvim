f17b8c4a9da24ff119a4ad7b37247591d362eb0a
Olivier Roos <olivier@MacBook-Pro-de-Olivier.local>
2024-07-31 23:42:36 +0200

    Add clipboard history controls

--- init.lua
+++ init.lua
@@ -1,17 +1,14 @@
 -- Lazy plugin manager pre-config
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit...", },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"},
+                           {"\nPress any key to exit..."}}, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -27,18 +24,22 @@ vim.opt.smartcase = true
 
 -- Plugins
 require("lazy").setup({
-  spec = {
-    -- Install pluggins here
-    "tpope/vim-surround",
-    "tpope/vim-repeat"
-  },
-  install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+    spec = { -- Install pluggins here
+    "tpope/vim-surround", "tpope/vim-repeat"},
+    install = {
+        colorscheme = {"habamax"}
+    },
+    checker = {
+        enabled = true
+    }
 })
 
--- Vim cmd
+-- Leader key shortcuts
 vim.cmd("nmap <leader>c :e ~/.config/nvim/init.lua<cr>") -- open init.lua config file
 vim.cmd("nmap <leader>s :w<cr>") -- better save file command
+vim.cmd("nmap <leader>xd <Cmd>lua require('vscode').action('clipboard-manager.history.clear')<CR>") -- clear clipboard history
+vim.cmd("nmap <leader>xh <Cmd>lua require('vscode').action('clipboard-manager.editor.pickAndPaste')<CR>") -- pick and paste from history
+
 -- Avoid unfold on navigation
 vim.cmd("nmap j gj")
 vim.cmd("nmap k gk")
