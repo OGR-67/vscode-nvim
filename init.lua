874570fd8aa8894716a0e79586f791cc55b7e5ae
OGR-67 <roosolivier@gmail.com>
2024-11-27 07:30:17 +0100

    Update keymaps to fit my IdeaVim config

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- Lazy plugin manager pre-config
+-- Lazy plugin manager pre-config local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,6 +15,9 @@ vim.opt.rtp:prepend(lazypath)
 -- Leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.keymap.set("v", "<Space>", "<Nop>", {
+    noremap = true
+})
 
 -- Vim opt
 vim.opt.clipboard = "unnamedplus" -- clipboard sync
@@ -34,29 +37,84 @@ require("lazy").setup({
     }
 })
 
--- Leader key shortcuts
-vim.cmd("nmap <leader>c :e ~/.config/nvim/init.lua<cr>") -- open init.lua config file
-vim.cmd("nmap <leader>s :w<cr>") -- better save file command
-vim.cmd("nmap <leader>xd <Cmd>lua require('vscode').action('clipboard-manager.history.clear')<CR>") -- clear clipboard history
-vim.cmd("nmap <leader>xh <Cmd>lua require('vscode').action('clipboard-manager.editor.pickAndPaste')<CR>") -- pick and paste from history
-vim.cmd("nmap <leader>w <Cmd>lua require('vscode').action('workbench.action.closeActiveEditor')<CR>") -- close tab
-vim.cmd("nmap <leader>z <Cmd>lua require('vscode').action('editor.toggleFold')<CR>") -- Toggle Fold
-vim.cmd("nmap <leader>f <Cmd>lua require('vscode').action('editor.action.formatDocument')<CR>") -- Format Document
-vim.cmd("nmap <leader>np <Cmd>lua require('vscode').action('editor.action.marker.nextInFiles')<CR>") -- Go to Next Problem
-vim.cmd("nmap <leader>pp <Cmd>lua require('vscode').action('editor.action.marker.prevInFiles')<CR>") -- Go to Previous Problem
-vim.cmd("nmap <leader>sf <Cmd>lua require('vscode').action('workbench.action.findInFiles')<CR>") -- Search In Files
+vim.cmd("nmap <leader>cc :e ~/.config/nvim/init.lua<cr>") -- open init.lua config file
 
-vim.cmd("nmap gr <Cmd>lua require('vscode').action('workbench.action.previousEditor')<CR>") -- Previous Tab
+vim.cmd("nmap <leader><leader> <Cmd>lua require('vscode').action('editor.action.formatDocument')<CR>") -- Format Document
+vim.cmd("nmap <leader>s :w<cr>") -- better save file command
+
+-- Tabs
+vim.cmd("nmap <leader>ww <Cmd>lua require('vscode').action('workbench.action.closeActiveEditor')<CR>") -- close tab
+vim.cmd("nmap <leader>pp <Cmd>lua require('vscode').action('workbench.action.previousEditor')<CR>") -- Previous Tab
+vim.cmd("nmap <leader>nn <Cmd>lua require('vscode').action('workbench.action.nextEditor')<CR>") -- Previous Tab
+
+-- Fold
+vim.cmd("nmap <leader>zz <Cmd>lua require('vscode').action('editor.toggleFold')<CR>") -- Toggle Fold
+vim.cmd("nmap <leader>za <Cmd>lua require('vscode').action('editor.unfoldAll')<CR>") -- Toggle All Fold
+
+-- Code Navigation
+vim.cmd("nmap <leader>en <Cmd>lua require('vscode').action('editor.action.marker.nextInFiles')<CR>") -- Go to Next Problem
+vim.cmd("nmap <leader>ep <Cmd>lua require('vscode').action('editor.action.marker.prevInFiles')<CR>") -- Go to Previous Problem
+
+vim.cmd("nmap <leader>uu <Cmd>lua require('vscode').action('editor.action.referenceSearch.trigger')<CR>") -- Go to References
+vim.cmd("nmap <leader>gl <Cmd>lua require('vscode').action('workbench.action.gotoLine')<CR>") -- Go to Line
+
+vim.cmd("nmap <leader>so <Cmd>lua require('vscode').action('outline.focus')<CR>") -- Go to Outline
+vim.cmd("nmap <leader>ss <Cmd>lua require('vscode').action('workbench.action.gotoSymbol')<CR>") -- Search Symbol
+
+vim.cmd("nmap <leader>ff <Cmd>lua require('vscode').action('workbench.action.findInFiles')<CR>") -- Search In Files
+vim.cmd("nmap <leader>fr <Cmd>lua require('vscode').action('workbench.action.replaceInFiles')<CR>") -- Search In Files
+
+vim.cmd("nmap <A-S-k> <Cmd>lua require('vscode').action('editor.action.showHover')<CR>") -- Show hover
+vim.cmd("nmap  <Cmd>lua require('vscode').action('editor.action.showHover')<CR>") -- Show hover
+
+-- Refactoring
+vim.cmd("nmap <leader>rn <Cmd>lua require('vscode').action('editor.action.rename')<CR>") -- Rename Symbol
+vim.cmd("nmap <leader>rr <Cmd>lua require('vscode').action('editor.action.quickFix')<CR>") -- Refactor List
+vim.cmd("nmap <leader>sw <Cmd>lua require('vscode').action('surround.with')<CR>") -- Surround extension is needed
+
+-- Lines
+vim.cmd("nmap <C-j> <Cmd>lua require('vscode').action('editor.action.moveLinesDownAction')<CR>") -- Move Line Down
+vim.cmd("nmap <C-k> <Cmd>lua require('vscode').action('editor.action.moveLinesUpAction')<CR>") -- Move Line Up
+vim.cmd("nmap <C-S-j> <Cmd>lua require('vscode').action('editor.action.copyLinesDownAction')<CR>") -- Duplicate Line
+
+-- Previous / Next position
+vim.cmd("nmap <S-k> <Cmd>lua require('vscode').action('workbench.action.navigateForward')<CR>")
+vim.cmd("nmap <S-j> <Cmd>lua require('vscode').action('workbench.action.navigateBack')<CR>")
 
 -- Avoid unfold on navigation
 vim.cmd("nmap j gj")
 vim.cmd("nmap k gk")
 
 -- Vim keymap
+vim.keymap.set("v", "s", "<Nop>", {
+    noremap = true,
+    silent = true
+}) -- allow surround with in visual mode
+
 vim.keymap.set("v", "p", "P") -- avoid clipboard replacement when parsing on visual mode
 vim.keymap.set("n", "U", "<C-r>") -- Better redo keymap
 vim.keymap.set("n", "<Esc>", ":nohlsearch<cr>") -- Escape to disable search highlighting
 vim.keymap.set("n", "m", "@") -- Use "m" to replay macros
+
+vim.keymap.set("n", "\\", '"_') -- Black whole register
+
+-- Mode normal et visuel combiné
+vim.keymap.set({'n', 'v'}, 'W', '[w', {
+    noremap = true,
+    silent = true
+})
+vim.keymap.set({'n', 'v'}, 'E', ']e', {
+    noremap = true,
+    silent = true
+})
+vim.keymap.set({'n', 'v'}, 'B', '[b', {
+    noremap = true,
+    silent = true
+})
+vim.keymap.set({'n', 'v'}, 'ge', ']b', {
+    noremap = true,
+    silent = true
+})
 
 -- Pre-enregistrer la macro dans le registre m
 vim.cmd('let @m = "viwP"')
